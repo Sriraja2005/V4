2735ce779fbfd7f310512fd281c10c05c815209a
Sriraja2005 <154749369+Sriraja2005@users.noreply.github.com>
2025-09-04 22:49:07 +0530

    Update settings change domain .py

--- company_website/settings.py
+++ company_website/settings.py
@@ -14,7 +14,11 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['v4smartsolutions.onrender.com',]
+ALLOWED_HOSTS = ['v4smartsolutions.onrender.com',
+                'www.v4smartsolutions.onrender.com',
+                 'onrender.com',
+                 'localhost'
+                ]
 
 # Application definition
 INSTALLED_APPS = [
