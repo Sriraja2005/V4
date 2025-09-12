a98cb462851233d258c2cfdef2a88dd22ac61318
Sriraja2005 <154749369+Sriraja2005@users.noreply.github.com>
2025-09-12 22:06:54 +0530

    Update settings.py

--- company_website/settings.py
+++ company_website/settings.py
@@ -14,7 +14,9 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['v4smartsolutions.onrender.com',
+                'localhost']
+
 
 # Application definition
 INSTALLED_APPS = [
