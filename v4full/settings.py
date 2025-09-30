c77d7dd71ba4e58b0e95fec33aa8a8c21f95b159
Sriraja2005 <154749369+Sriraja2005@users.noreply.github.com>
2025-10-01 01:40:53 +0530

    Update settings.py

--- v4full/settings.py
+++ v4full/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = [
 ]
 CSRF_TRUSTED_ORIGINS = ['http://localhost:8000',
     'http://127.0.0.1:8000',
-'http://v4smartsolutions.onrender.com', ]
+'https://v4smartsolutions.onrender.com', ]
 # Application definition
 
 INSTALLED_APPS = [
